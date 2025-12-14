0f944ad175ac05c315992609b8ad8c70b9dc82e5
bratpeki <pkatic2003@gmail.com>
2025-12-14 19:19:36 +0100

    More additions to vector.c

--- src/vector.c
+++ src/vector.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /*
  * This is an example of an integer vector.
@@ -38,13 +39,33 @@ typedef struct fvec {
  */
 #define VEC_APPEND( vec, item ) \
 	do { \
-		if ( vec.count >= vec.capacity ) { \
-			if ( vec.capacity == 0 ) vec.capacity = 16; \
-			else vec.capacity *= 2; \
-			vec.items = realloc(vec.items, vec.capacity * sizeof(*vec.items)); \
-			if ( vec.items == NULL ) exit(1); \
+		if ( (vec).count >= (vec).capacity ) { \
+			if ( (vec).capacity == 0 ) (vec).capacity = 16; \
+			else (vec).capacity *= 2; \
+			(vec).items = realloc((vec).items, (vec).capacity * sizeof(*(vec).items)); \
+			if ( (vec).items == NULL ) exit(1); \
 		} \
-		vec.items[vec.count++] = item; \
+		(vec).items[(vec).count++] = (item); \
+	} while (0)
+
+/*
+ * Removes an item from `vec` at the given `index`.
+ *
+ * Checks the upper bound (always the index of the last added item).
+ *
+ * Make sure `index` is an unsigned type, since this macro doesn't check the lower bound.
+ * Preferably, `index` is a `size_t`.
+ */
+#define VEC_REMOVE( vec, index ) \
+	do { \
+		if ( (index) < (vec).count ) { \
+			memmove( \
+				&(vec).items[(index)], \
+				&(vec).items[(index) + 1], \
+				sizeof(*(vec).items) * ((vec).count - 1 - (index)) \
+			); \
+			(vec).count--; \
+		} \
 	} while (0)
 
 int main() {
@@ -61,17 +82,33 @@ int main() {
 	VEC_APPEND(iv, 4);
 	VEC_APPEND(iv, 5);
 
+	/* (0:1), (1:2), (2:3), (3:4), (4:5) */
+	for ( i = 0; i < iv.count; i++ )
+		printf("(%lu:%d) ", i, iv.items[i]);
+	printf("\n");
+
+	VEC_REMOVE(iv, 0); /* Removes 1 */
+	VEC_REMOVE(iv, 0); /* Removes 2 */
+	VEC_REMOVE(iv, iv.count - 1); /* Removes 5 */
+
+	/* (0:3) (1:4) */
+	for ( i = 0; i < iv.count; i++ )
+		printf("(%lu:%d) ", i, iv.items[i]);
+	printf("\n");
+
 	VEC_APPEND(fv, 1.5f);
 	VEC_APPEND(fv, 2.5f);
 	VEC_APPEND(fv, 3.5f);
 	VEC_APPEND(fv, 4.5f);
 	VEC_APPEND(fv, 5.5f);
 
-	for ( i = 0; i < iv.count; i++ )
-		printf("iv[%lu] = %d\n", i, iv.items[i]);
-
+	/* (0:1.5) (1:2.5) (2:3.5) (3:4.5) (4:5.5) */
 	for ( i = 0; i < fv.count; i++ )
-		printf("fv[%lu] = %f\n", i, fv.items[i]);
+		printf("(%lu:%.1f) ", i, fv.items[i]);
+	printf("\n");
+
+	free(iv.items);
+	free(fv.items);
 
 	return EXIT_SUCCESS;
 
